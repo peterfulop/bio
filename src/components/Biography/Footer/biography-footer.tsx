8aaacd4da46cefbcd3aec9eabfd498387688bea1
peterfulop <mrpeterfulop@gmail.com>
2022-04-04 11:09:35 +0200

    fix(*):removed some unnecessary component;

--- src/components/Biography/Footer/biography-footer.tsx
+++ src/components/Biography/Footer/biography-footer.tsx
@@ -1,6 +1,5 @@
 import { IconNames } from "../../../utils/cv.interface";
 import TechStack from "../../TechStack/tech-stack";
-import "./biography-footer.css";
 
 export default function BiographyHeader(props: { techStack: IconNames[] }) {
   return (
